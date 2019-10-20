671ca27f6b9fc048fd981cfb55a4dd987e630795
Eric Glover <etgt28@mail.missouri.edu>
2019-10-20 01:20:44 -0500

    bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 var path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   context: path.resolve(__dirname),
   entry: {
     main: "./web/js/pages/index.js",
     profile: "./web/js/pages/profile.js",
-    superStarTrek: "./web/js/games/superStarTrek.js"
+    superStarTrek: "./web/js/games/superStarTrek/superStarTrek.js"
   },
   output: {
     path: path.resolve(__dirname, "web/dist"),
